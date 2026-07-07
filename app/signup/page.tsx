7a295017f8dcd1a683d552fbd209b4a23dbb7429
Divyanshu Singh <divyanshu@Divyanshus-MacBook-Air.local>
2026-07-07 15:34:32 +0530

    fix: use hard navigation and fix PageTransition hydration to fix redirect loops

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,9 +7,9 @@ export default function SignUpRoute() {
   const router = useRouter();
 
   const handleNavigate = (view: string) => {
-    if (view === "landing") router.push("/");
-    else if (view === "study-material-page") router.push("/study-material");
-    else router.push("/" + view);
+    if (view === "landing") window.location.href = "/";
+    else if (view === "study-material-page") window.location.href = "/study-material";
+    else window.location.href = "/" + view;
   };
 
   return <SignUpPage setCurrentView={handleNavigate} />;
